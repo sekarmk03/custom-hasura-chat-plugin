d156cec627385de994fcd80eb5b7220ea4f0d25c
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2025-03-10 17:57:06 +0700

    fix: import func middleware

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const preParse = require('./pre-parse');
 const preResponse = require('./pre-response');
 const validateHasuraPluginSecret = require('../middlewares/graphql-auth');
 
-router.use('/pre-parse', validateHasuraPluginSecret, preParse);
+router.use('/pre-parse', validateHasuraPluginSecret(), preParse);
 router.use('/pre-response', preResponse);
 
 module.exports = router;

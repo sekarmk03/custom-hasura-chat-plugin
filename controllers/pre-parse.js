dde5dd6228076a366cfd097278eb08595346ac26
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2025-03-10 20:02:34 +0700

    feat: update validator content

--- controllers/pre-parse.js
+++ controllers/pre-parse.js
@@ -2,7 +2,7 @@ const err = require('../utils/common/errors');
 const requestValidator = require('../utils/validator/request');
 const Validator = require('fastest-validator');
 const v = new Validator();
-const { argumentParser, extractContent } = require('../utils/common/object-parser');
+const { extractContent } = require('../utils/common/object-parser');
 
 module.exports = {
     validation: async (req, res, next) => {
@@ -16,17 +16,14 @@ module.exports = {
                     break;
                 case "CreateNewMessage":
                     console.log(query);
-                    const parsed = argumentParser(query);
-
-                    console.log("Parsed: " + parsed);
 
                     const content = extractContent(query);
                     console.log("Content: " + content);
 
-                    // const val = v.validate(parsed, requestValidator.createMessage);
-                    // console.log(val);
+                    const val = v.validate({content}, requestValidator.createMessage);
+                    console.log(val);
                     
-                    // if (val.length) return err.bad_request(res, val[0].message);
+                    if (val.length) return err.bad_request(res, val[0].message);
                     
                     return res.status(200).json({
                         data: {

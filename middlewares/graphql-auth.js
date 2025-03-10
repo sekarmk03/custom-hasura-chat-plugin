8042dd5cf1c4f945eaff9544adbd495d092d2d1b
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2025-03-10 17:40:34 +0700

    fix: import func middleware

--- middlewares/graphql-auth.js
+++ middlewares/graphql-auth.js
@@ -1,20 +1,20 @@
 const { HASURA_PLUGIN_SECRET } = process.env;
 const err = require('../utils/common/errors');
 
-const validateHasuraPluginSecret = (req, res, next) => {
-    const authHeader = req.headers['hasura-m-auth'];
-
-    if (!authHeader) return err.unauthorized(res, 'Access denied. No hasura-m-auth provided');
-
-    try {
-        if (authHeader !== HASURA_PLUGIN_SECRET) {
-            return err.unauthorized(res, 'Access denied. Invalid hasura-m-auth');
-        }
+module.exports = () => {
+    return (req, res, next) => {
+        const authHeader = req.headers['hasura-m-auth'];
     
-        next();
-    } catch (error) {
-        return err.unauthorized(res, 'Invalid token');
+        if (!authHeader) return err.unauthorized(res, 'Access denied. No hasura-m-auth provided');
+    
+        try {
+            if (authHeader !== HASURA_PLUGIN_SECRET) {
+                return err.unauthorized(res, 'Access denied. Invalid hasura-m-auth');
+            }
+        
+            next();
+        } catch (error) {
+            return err.unauthorized(res, 'Invalid token');
+        }
     }
-}
-
-module.exports = validateHasuraPluginSecret;
+};

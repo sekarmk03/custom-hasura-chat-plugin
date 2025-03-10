af7b6ff8d9c6f867501f00825a3557dbd7cac2a1
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2025-03-10 19:44:04 +0700

    feat: add test validator

--- controllers/pre-parse.js
+++ controllers/pre-parse.js
@@ -1,5 +1,5 @@
 const err = require('../utils/common/errors');
-const reqValidator = require('../utils/validator/request');
+const requestValidator = require('../utils/validator/request');
 const Validator = require('fastest-validator');
 const v = new Validator();
 const { argumentParser } = require('../utils/common/object-parser');
@@ -18,8 +18,11 @@ module.exports = {
                     console.log(query);
                     const parsed = argumentParser(query);
                     console.log(parsed);
-                    console.log(parsed.chatId);
-                    console.log(parsed.content);
+
+                    const val = v.validate(parsed, requestValidator.createMessage);
+                    console.log(val);
+                    
+                    if (val.length) return err.bad_request(res, val[0].message);
                     
                     return res.status(200).json({
                         data: {

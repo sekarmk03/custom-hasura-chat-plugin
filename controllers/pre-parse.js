d448ba5d5dc66278ca1f6888d12ae14e5a13e182
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2025-03-11 12:28:45 +0700

    feat: add return send response

--- controllers/pre-parse.js
+++ controllers/pre-parse.js
@@ -3,6 +3,7 @@ const requestValidator = require('../utils/validator/request');
 const Validator = require('fastest-validator');
 const v = new Validator();
 const { extractContent } = require('../utils/common/object-parser');
+const { JWT_SECRET_KEY } = process.env;
 
 module.exports = {
     validation: async (req, res, next) => {
@@ -21,25 +22,39 @@ module.exports = {
                     console.log("Content: " + content);
 
                     const val = v.validate({content}, requestValidator.createMessage);
-                    console.log(val);
-                    
                     if (val.length) return err.bad_request(res, val[0].message);
-                    
-                    return res.status(200).json({
-                        data: {
-                            validate: true,
-                            message: 'Validation success',
-                            rawRequest: rawRequest,
-                        },
-                  });
                     break;
                 default:
                     break;
             }
+
+            return res.status(204).send();
         } catch (error) {
             console.log(error);
-            
             return err.internal(res, error.message);
         }
+    },
+
+    auth: async (req, res, next) => {
+        const token = req.headers['authorization']?.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized: No token provided' });
+        }
+
+        jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+            }
+
+            const sessionVariables = {
+                'x-hasura-user-id': decoded.userId,
+                'x-hasura-role': decoded.role,
+            };
+
+            req.body.session = sessionVariables;
+
+            return res.status(204).send();
+        });
     }
 };
---
+++ middlewares/authorize.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET_KEY } = process.env;
+const err = require('../utils/errors');
+
+module.exports = (roles = []) => {
+    // if passed role only 1 integer
+    if (typeof roles === 'number') roles = [roles];
+
+    return (req, res, next) => {
+        let token = req.header('Authorization');
+        if (!token) return err.unauthorized(res, 'Access denied. No token provided');
+        token = token.split(' ')[1];
+
+        try {
+            const payload = jwt.verify(token, JWT_SECRET_KEY);
+
+            req.user = payload;
+
+            next();
+        } catch (error) {
+            return err.unauthorized(res, 'Invalid token');
+        }
+    }
+}
